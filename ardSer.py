43c8425cda14fcc78ef0c7d782c822d5fba83d28
avinash1612 <56393465+avinash1612@users.noreply.github.com>
2020-10-04 02:17:17 +0530

    Update ardSer.py

--- ardSer.py
+++ ardSer.py
@@ -1,18 +1,16 @@
 import serial
 import time
-print('This program allows a user to turn an LED on and off')
-print('type H to turn the LED on')
-print('type L to turn the LED off')
-print('type q to quit')
-ser = serial.Serial('COM4', 9800, timeout=1)
-time.sleep(2)
+print('This program allows a user to turn an LED on and on')
+ser = serial.Serial('COM4', 115200, timeout=1)
+time.sleep(20)
 
 user_input = 'L'
-while user_input != 'q':
+while user_input != 'r':
     user_input = input('H = on, L = off, q = quit :' )
     byte_command = user_input.encode()
     ser.writelines(byte_command)   # send a byte
-    time.sleep(0.5) # wait 0.5 seconds
+    time.sleep(0.52) # wait 0.5 seconds
 
 print('q entered. Exiting the program')
 ser.close()
+print("done")
